772c3da7bfef98d2855f24cf8c8ff966a5f2ecd0
KeyllaCastro <keylla.castro@etec.sp.gov.br>
2024-12-04 13:43:08 -0300

    Adicionando arquivos doisNumeros.js e index.html

---
+++ doisNumeros.js
@@ -0,0 +1,10 @@
+//Ler dois números e mostre o maior
+//Digitar numero
+var numero = parseFloat(prompt("Digite um número?"))
+var numero2 = parseFloat(prompt("Digite outro número?"))
+//Mostrando qual numero é maior
+if (numero > numero2){
+    alert("O primeiro numero é maior");
+}else{
+    alert("O segundo numero é maior")
+}
